cc84f27c32f3bb409eba65aa0ef4b7429cba87cd
candrade <chris.andrade@gmail.com>
2016-08-15 16:08:18 -0700

    email sign-up fix

--- js/musicmind_legacy.js
+++ js/musicmind_legacy.js
@@ -66,8 +66,18 @@ $(function () {
     });
 
     // CTA Init
-    $("#signup-form").submit(onSubmit);
-    $("#final-signup-form").submit(onSubmit);
+    $("#signup-form").submit(function(e) {
+      e.preventDefault();
+    });
+    $("#final-signup-form").submit(function(e) {
+      e.preventDefault();
+      onSubmit($(e.currentTarget));
+    });
+
+    $("#yes-button").click(function (e) {
+      e.preventDefault();
+      onSubmit($("#signup-form"));
+    });
 
     $("#no-button").click(function (e) {
       e.preventDefault();
@@ -308,9 +318,9 @@ $(function () {
     });
   }
 
-  function onSubmit(e) {
-    e.preventDefault();
-    var $form = $(e.currentTarget);
+  function onSubmit($form) {
+    // e.preventDefault();
+    // var $form = $(e.currentTarget);
     var $emailField = $form.find("input[name=EMAIL]");
     var $errMessage = $form.find("#error-response");
 
